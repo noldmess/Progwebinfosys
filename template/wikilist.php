fe42956609d0316c981babd02c4dcdc75b14a43b
aaron <aaron@ubuntu-aaron>
2013-04-13 08:39:21 +0200

    add /

--- template/wikilist.php
+++ template/wikilist.php
@@ -3,9 +3,9 @@
 	<ul class="nav nav-list">
 		<li class="nav-header">Sidebar</li>
 		<?php foreach($TEMPLATE['index'] as $item){?>
-			<li><a href="wiki/<?php echo urlencode($item);?>/"> <?php echo urldecode($item);?></a></li>
+			<li><a href="/wiki/<?php echo urlencode($item);?>/"> <?php echo urldecode($item);?></a></li>
 		<?php }?>
-	<li><a href="wiki/new/"><i class="icon-pencil"></i>Neuer Artikel</a></li>
+	<li><a href="/wiki/new/"><i class="icon-pencil"></i>Neuer Artikel</a></li>
 	</ul>
 	</div>
 	<!--/.well -->
