2346cfb6d21549481bfc2c2134cfbef8e8f3b22d
aaron <aaron@ubuntu-aaron>
2013-04-13 08:29:16 +0200

    das

--- template/newarticle.php
+++ template/newarticle.php
@@ -6,7 +6,7 @@
 					<input  name="title" type="text" placeholder="Type something…" value="<?php echo (isset($_GET['title'])?$_GET['title']:""); ?>">
 					<span class="help-block">Description</span>
 					<label class="checkbox">
-						<textarea name="text" rows="3"><?php if($_GET['action']==="change"){echo $_SESSION[$_GET['title']];}?></textarea>
+						<textarea name="text" rows="3"><?php if($_GET['action']==="change"){echo $TEMPLATE['text'];}?></textarea>
 					</label>
 					<button type="submit" class="btn">Submit</button>
 			</fieldset>
