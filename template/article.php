f599edd17cb446c0f6a6b29723a4e486e16d6de3
Markus Mueller <csam5409@student.uibk.ac.at>
2013-04-16 22:30:03 +0200

    startseite wiki fix

--- template/article.php
+++ template/article.php
@@ -1,6 +1,6 @@
 <div class="span9">
 	<div class="hero-unit">
-<?php if(isset($TEMPLATE['title'])){?>
+<?php if(isset($TEMPLATE['title']) && !empty($TEMPLATE['title'])){?>
 		<h1><?php echo urldecode($TEMPLATE['title'])?></h1>
 		<p><?php echo $TEMPLATE['text']?></p>
 		<a class="btn btn-danger" href="/wiki/<?php echo urlencode($TEMPLATE['title']);?>/remove"><i class="icon-remove"></i>remove</a>
