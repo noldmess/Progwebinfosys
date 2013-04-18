a4f9ca886def755ebd57ac9cf3ea7c31df7f0866
aaron <aaron@ubuntu-aaron>
2013-04-18 14:43:55 +0200

    add jquery + search

---
+++ template/searchResult.php
@@ -0,0 +1,21 @@
+ <div class="span9">
+   <div class="hero-unit">
+ 	<h3>Search for:<?php echo $TEMPLATE['searchTest'];?></h3>
+	
+			
+			<?php
+				if(sizeof($TEMPLATE['searchList'])<=0){
+			?>
+					<li>No Result</li>
+			<?php 
+				}else{
+					foreach($TEMPLATE['searchList'] as $item){
+			?>
+					<li><a href="/PvW/wiki/<?php echo urlencode($item);?>/"> <?php echo urldecode($item);?></a></li>
+			<?php 
+					}
+				}
+			?>
+			
+	</div>
+</div>
