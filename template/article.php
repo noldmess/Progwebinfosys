d668b359e4c39f8b1df4be9d260324792575713c
Markus Mueller <csam5409@student.uibk.ac.at>
2013-04-22 12:25:46 +0200

    using parsedText

--- template/article.php
+++ template/article.php
@@ -1,7 +1,7 @@
 <div class="span9">
 	<div class="hero-unit">
 		<h1><?php echo urldecode($TEMPLATE['title'])?></h1>
-		<p><?php echo $TEMPLATE['text']?></p>
+		<p><?php echo $TEMPLATE['parsedText']?></p>
 		<a class="btn btn-danger" href="/wiki/<?php echo urlencode($TEMPLATE['title']);?>/remove"><i class="icon-remove"></i>remove</a>
 		<a class="btn btn-primary" href="/wiki/<?php echo urlencode($TEMPLATE['title']);?>/change"><i class="icon-pencil"></i>change</a>
         <?php foreach($TEMPLATE['linklist'] as $item){?>
