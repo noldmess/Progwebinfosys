2d8fb7d5553cf730b168bcf28cf85c6033421909
aaron <aaron@ubuntu-aaron>
2013-04-27 08:36:53 +0200

    login

--- template/newarticle.php
+++ template/newarticle.php
@@ -1,10 +1,10 @@
 <div class="span9">
 	<div class="hero-unit">
-		<form  class="form-horizontal" action="/wiki/<?php echo (isset($_GET['title'])?urlencode($_GET['title'])."/":""); ?>" method="post">
+		<form  class="form-horizontal" action="<?php echo $_SERVER['HTTP_REFERER'];?> /wiki/<?php echo (isset($_GET['title'])?urlencode($_GET['title'])."/":""); ?>" method="post">
 			<fieldset>
 				<label>Title</label>
                     <input name="id" type="hidden" value="<?php if($_GET['action']==="change"){echo $TEMPLATE['id'];}?>" />
-					<input name="title" type="text" placeholder="Type something…" value="<?php echo (isset($TEMPLATE['title'])?$TEMPLATE['title']:$_GET['title']); ?>">
+					<input name="title" type="text" placeholder="Type something…" value="<?php echo $TEMPLATE['title']; ?>">
 					<span class="help-block">Description</span>
 					<label class="checkbox">
 						<textarea name="text" rows="3"><?php if($_GET['action']==="change"){echo $TEMPLATE['text'];}?></textarea>
