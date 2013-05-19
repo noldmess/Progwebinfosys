2a0290663c84224c00e804045cca1e762323068e
noldmess <aaron.messner@student.uibk.ac.at>
2013-05-19 11:08:09 +0300

    Update GameForm.php

--- Zend/module/Game/src/Game/Form/GameForm.php
+++ Zend/module/Game/src/Game/Form/GameForm.php
@@ -62,7 +62,7 @@ class GameForm extends Form
             'attributes' => array(
                 'value' => 'Go',
                 'id' => 'submitbutton',
-                'onclick' => 'alert("Hello!")',
+                'onsubmit' => 'onsubmittest()',
             ),
         ));
     }
