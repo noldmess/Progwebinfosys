bbcc124e67c7d4bb416de45ae57d63c27e473f1e
aaron <aaron@ubuntu-aaron>
2013-05-24 19:46:39 +0200

    dsfsdf

--- Zend/module/Game/src/Game/Controller/GameController.php
+++ Zend/module/Game/src/Game/Controller/GameController.php
@@ -256,7 +256,7 @@ class GameController extends AbstractActionController
 				//$this->getGameTable()->saveGame($game);
 				
 				$document = $game->getDocument();
-				echo json_endode($document);
+				echo json_decode($document);
 				$this->getDb()->games->save($document);
 					
 				$msg = 'Your oppononent, '.$game->user2.', has chosen his weapon. To see the result click';
