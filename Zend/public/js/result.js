6c31be259176b32f163e49b9b91d7583626576af
noldmess <aaron.messner@student.uibk.ac.at>
2013-05-28 09:00:37 +0300

    Update result.js

--- Zend/public/js/result.js
+++ Zend/public/js/result.js
@@ -1,14 +1,13 @@
-
 var Result={
 		result:function(hashcode,container){
 			var container=container;
-			$.post('http://138.232.66.90/Zend/public/game/resultJSON',{hash:hashcode}, function(data) {
+			$.post('/Zend/public/game/resultJSON',{hash:hashcode}, function(data) {
 					$('h1').remove();
 					$(container).append("<h1>Result</h1>");
 					$(container).append("<p>Player 1: "+data.game.user1+""+data.game.email1+"<br  />");
-					$(container).append("Player 1: "+data.game.choice1+" <br  /></p>");
+					$(container).append("Player 1: "+data.game.choice1+" </p>");
 					$(container).append("<p>Player 2: "+data.game.user2+""+data.game.email2+" <br  />");
-					$(container).append("Player 2: "+data.game.choice2+" <br  /></p>");
+					$(container).append("Player 2: "+data.game.choice2+" </p>");
 					$(container).append("<p>Result : "+data.game.result+" <br  /></p>");
 					$(container).append('<a class="btn btn-primary">Revenge</a><br> ');
 					$(container+" a").click(function(){
