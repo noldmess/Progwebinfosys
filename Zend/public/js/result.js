e05c4c21dabbd370e5557e5b2563bd78e83b4fae
mills7 <mueller.markus@gmx.com>
2013-05-28 21:32:50 +0200

    Update result.js

--- Zend/public/js/result.js
+++ Zend/public/js/result.js
@@ -5,9 +5,9 @@ var Result={
 					$('h1').remove();
 					$(container).append("<h1>Result</h1>");
 					$(container).append("<p>Message from player 2:<br>"+data.game.msg2+"</p>");
-					$(container).append("<p>Player 1: "+data.game.user1+""+data.game.email1+"</p>");
+					$(container).append("<p>Player 1: "+data.game.user1+", "+data.game.email1+"</p>");
 					$(container).append("<p>Player 1: "+data.game.choice1+" </p>");
-					$(container).append("<p>Player 2: "+data.game.user2+""+data.game.email2+" </p>");
+					$(container).append("<p>Player 2: "+data.game.user2+", "+data.game.email2+" </p>");
 					$(container).append("<p>Player 2: "+data.game.choice2+" </p>");
 					$(container).append("<p>Result : "+data.game.result+" </p>");
 					$(container).append('<a class="btn btn-primary">Revenge</a><br> ');
