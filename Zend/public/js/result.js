f5cc90fa4b15d9273873bff9ba2fa8dbffb67b84
noldmess <aaron.messner@student.uibk.ac.at>
2013-05-28 08:04:24 +0200

    Update result.js

--- Zend/public/js/result.js
+++ Zend/public/js/result.js
@@ -4,11 +4,11 @@ var Result={
 			$.post('/Zend/public/game/resultJSON',{hash:hashcode}, function(data) {
 					$('h1').remove();
 					$(container).append("<h1>Result</h1>");
-					$(container).append("<p>Player 1: "+data.game.user1+""+data.game.email1+"<br  />");
-					$(container).append("Player 1: "+data.game.choice1+" </p>");
-					$(container).append("<p>Player 2: "+data.game.user2+""+data.game.email2+" <br  />");
-					$(container).append("Player 2: "+data.game.choice2+" </p>");
-					$(container).append("<p>Result : "+data.game.result+" <br  /></p>");
+					$(container).append("<p>Player 1: "+data.game.user1+""+data.game.email1+"</p>");
+					$(container).append("<p>Player 1: "+data.game.choice1+" </p>");
+					$(container).append("<p>Player 2: "+data.game.user2+""+data.game.email2+" </p>");
+					$(container).append("<p>Player 2: "+data.game.choice2+" </p>");
+					$(container).append("<p>Result : "+data.game.result+" </p>");
 					$(container).append('<a class="btn btn-primary">Revenge</a><br> ');
 					$(container+" a").click(function(){
 						Revange.result(hashcode,'#div');
