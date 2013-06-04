76e93df93139c565ea1c597527d436f0a290a287
mills7 <mueller.markus@gmx.com>
2013-06-04 22:14:24 +0300

    Update result.js

--- Zend3/public/js/result.js
+++ Zend3/public/js/result.js
@@ -5,13 +5,13 @@ var Result={
 			var container=container;
 			$.post('/zend3/game/resultJSON',{hash:hashcode,player:player}, function(data) {
 				$('#resultuser1').empty();
-				$('#resultuser1msg').empty();
+				$('#resultuser2msg').empty();
 				$('#resultuser2').empty();
 				$('#resultchoice1').empty();
 				$('#resultchoice2').empty();
 				$('#resulttxt').empty();
 				$('#resultuser1').append("Player 1:"+data.game.user1+", "+data.game.email1);
-				$('#resultuser1msg').append(data.game.msg2);
+				$('#resultuser2msg').append(data.game.msg2);
 				$('#resultuser2').append("Player 2:"+data.game.user2+", "+data.game.email2);
 				$('#resultchoice1').append("Player 1: "+data.game.choice1);
 				$('#resultchoice2').append("Player 2: "+data.game.choice2);
